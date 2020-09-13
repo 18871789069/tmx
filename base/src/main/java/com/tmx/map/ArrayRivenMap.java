63fc121bec4d0662baf20327a479642004255283
guanweiquan <guan_weiquan@ctg.com.cn>
2020-09-13 15:54:21 +0800

    fixed 补充ArrayListMap删除的方法

--- base/src/main/java/com/tmx/map/ArrayRivenMap.java
+++ base/src/main/java/com/tmx/map/ArrayRivenMap.java
@@ -11,6 +11,7 @@ public class ArrayRivenMap<K,V> {
 
     List<Entry<K,V>> entryList = new ArrayList<>();
 
+    // 新增和修改
     public void put(K key,V value) {
         Entry<K,V> entry = getEntry(key);
         if (entry != null) {
@@ -24,6 +25,19 @@ public class ArrayRivenMap<K,V> {
     public V get(K key) {
         Entry<K,V> entry = getEntry(key);
         return entry == null ? null: entry.value;
+    }
+
+    // 删除
+    public V remove(K key) {
+        Entry<K, V> oldEntry = getEntry(key);
+        for (int i = 0; i < entryList.size(); i++) {
+            Entry<K, V> kvEntry = entryList.get(i);
+            if (kvEntry.key.equals(key)) {
+                entryList.remove(i);
+                break;
+            }
+        }
+        return oldEntry.value;
     }
 
     private Entry<K,V> getEntry(K key) {
@@ -44,13 +58,6 @@ public class ArrayRivenMap<K,V> {
             this.key = key;
             this.value = value;
         }
-    }
-
-    public static void main(String[] args) {
-        ArrayRivenMap<String, String> arrayRivenMap = new ArrayRivenMap<>();
-        arrayRivenMap.put("a","aaaaa");
-        arrayRivenMap.put("b","bbbbb");
-        System.out.println(arrayRivenMap.get("c"));
     }
 }
 
