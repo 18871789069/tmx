c7cbbb947a090d291faa048b3870a0f4c42bf883
guanweiquan <guan_weiquan@ctg.com.cn>
2020-09-14 17:23:55 +0800

    重新提交

--- base/src/test/java/com/tmx/MapTest.java
+++ base/src/test/java/com/tmx/MapTest.java
@@ -1,6 +1,6 @@
 package com.tmx;
 
-import com.tmx.list.arrayList.RivenArrayLsit;
+import com.tmx.list.linkedList.LinkedList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * Created By Riven on 2020-8-27
@@ -20,20 +21,33 @@ public class MapTest {
 
     @Test
     public void test() {
-//        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 //        map.put("string", "测试");
-//        int i = 16 >> 4;
-//        System.out.println("1 << 4 等于" + Integer.toString(i));
-
-        RivenArrayLsit rivenArrayLsit = new RivenArrayLsit();
-        rivenArrayLsit.add("String");
-        rivenArrayLsit.add("1111111");
-        for (int i = 0; i < rivenArrayLsit.getSize(); i++) {
-            System.out.println(rivenArrayLsit.get(i));
+//        int nei = 16 >> 4;
+//        System.out.println("1 << 4 等于" + Integer.toString(nei));
+//
+//        RivenArrayLsit rivenArrayLsit = new RivenArrayLsit();
+//        rivenArrayLsit.add("String");
+//        rivenArrayLsit.add("1111111");
+//        for (int i = 0; i < rivenArrayLsit.getSize(); i++) {
+//            System.out.println(rivenArrayLsit.get(i));
+//        }
+//        rivenArrayLsit.set(1,"new String");
+//        rivenArrayLsit.remove(0);
+//        for (int i = 0; i < rivenArrayLsit.getSize(); i++) {
+//            System.out.println("新生成的数据" + rivenArrayLsit.get(i));
+//        }
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("string");
+        linkedList.add("11111");
+        linkedList.add("22222");
+        linkedList.add("33333");
+        linkedList.add("44444");
+        linkedList.add(1, "abc");
+        linkedList.add(3, "55555");
+        for (int i = 0; i < linkedList.getSize(); i++) {
+            System.out.println(linkedList.get(i));
         }
-        rivenArrayLsit.set(1,"new String");
-        for (int i = 0; i < rivenArrayLsit.getSize(); i++) {
-            System.out.println("新生成的数据" + rivenArrayLsit.get(i));
-        }
+//        LinkedList
     }
 }
